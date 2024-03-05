e71300b6d1a6dd1719b5bd64b5f23d20381b447d
teoMarinov <teodor.b.marinov@gmail.com>
2024-03-05 11:56:37 +0200

    change size of skills icons

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,54 +1,11 @@
-import typeScript from "../../assets/SkillsIcons/typeScript.png"
-import javaScript from "../../assets/SkillsIcons/javaScript.png"
-import html from "../../assets/SkillsIcons/html.png"
-import css from "../../assets/SkillsIcons/css.png"
-import php from "../../assets/SkillsIcons/php.png"
-import sql from "../../assets/SkillsIcons/sql.png"
-import react from "../../assets/SkillsIcons/react.png"
-import nextJs from "../../assets/SkillsIcons/nextjs.png"
-import reactNative from "../../assets/SkillsIcons/reactNative.png"
-import framerMotion from "../../assets/SkillsIcons/framerMotion.svg"
-import tailwind from "../../assets/SkillsIcons/tailwind.png"
-import chakraUi from "../../assets/SkillsIcons/chakraUI.png"
-import shadcn from "../../assets/SkillsIcons/shadcn.png"
-import firebase from "../../assets/SkillsIcons/firebase.png"
-import prisma from "../../assets/SkillsIcons/prisma.png"
-import mongo from "../../assets/SkillsIcons/mongo.png"
-import postgress from "../../assets/SkillsIcons/postgresql.png"
-import mysql from '../../assets/SkillsIcons/mysql.svg'
-import ajax from "../../assets/SkillsIcons/ajax.png"
-import webSocket from "../../assets/SkillsIcons/websocket.png"
-import nodeJs from "../../assets/SkillsIcons/nodeJS.png"
+import SkillsRow from "./SkillsRow"
 
 const Skills = () => {
     return (
         <div className="sm:h-screen h-fit md:my-0 mt-14 mb-44">
             <h1 className="w-screen text-center 2xl:text-9xl sm:text-8xl text-6xl font-poppins font-extralight sm:mb-16 mb-8">Skills</h1>
-            <div className="flex gap-x-5 2xl:size-60 md:size-40 sm:size-32 size-20 2xl:w-full md:w-screen sm:w-full w-screen overflow-y-auto mb-16">
-                <img src={typeScript} alt="type-script" />
-                <img src={javaScript} alt="java-script" className="" />
-                <img src={html} alt="html" className="" />
-                <img src={css} alt="css" className="" />
-                <img src={nextJs} alt="nextJs" className="mt-2" />
-                <img src={react} alt="react" className="" />
-                <img src={reactNative} alt="react-native" className="mt-2" />
-                <img src={php} alt="php" className="" />
-                <img src={sql} alt="sql" className="" />
-                <img src={framerMotion} alt="framer-motion" className="mt-2" />
-            </div>
-            <div className="flex gap-5 2xl:size-60 md:size-40 sm:size-32 size-20 2xl:w-full md:w-screen sm:w-full w-screen overflow-y-auto mb-16">
-                <img src={tailwind} alt="tailwind" className="mt-2" />
-                <img src={chakraUi} alt="chakraUI" className="mt-2" />
-                <img src={shadcn} alt="shadcn" className="mt-2 rounded-lg" />
-                <img src={firebase} alt="firebase" className="mt-2" />
-                <img src={prisma} alt="prisma" className="mt-4" />
-                <img src={mongo} alt="mongo" className="mt-4" />
-                <img src={postgress} alt="postgress" className="mt-4" />
-                <img src={mysql} alt="mysql" className="mt-4" />
-                <img src={ajax} alt="ajax" className="mt-4" />
-                <img src={webSocket} alt="webSocket" className="mt-4" />
-                <img src={nodeJs} alt="nodeJs" className="mt-4" />
-            </div>
+            <SkillsRow/>
+            <SkillsRow/>
         </div>
     )
 }
