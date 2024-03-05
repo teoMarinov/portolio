b17b9df848e5c7177a440e0af6d07f765abcada6
teoMarinov <teodor.b.marinov@gmail.com>
2024-03-05 11:53:23 +0200

    finish desktop contact me design

--- src/components/ContactMe/ContactForm.jsx
+++ src/components/ContactMe/ContactForm.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react"
-import clsx from "clsx";
 import Input from "./Input";
+import TextArea from "./TextArea";
 
 const ContactForm = () => {
 
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('a');
+    const [message, setMessage] = useState('');
     const [honey, setHoney] = useState('');
     const [errors, setErrors] = useState({
         name: '',
@@ -36,7 +36,7 @@ const ContactForm = () => {
             setErrors(prevErrors => ({ ...prevErrors, message: "Please enter your message" }));
         }
         if (errors.name || errors.email || errors.message) return;
-    
+
 
 
         console.log({ name, email, message });
@@ -44,16 +44,12 @@ const ContactForm = () => {
 
 
     return (
-        <form className="flex gap-16 flex-col w-2/3" onSubmit={handleSubmit}>
+        <form className="flexflex-col w-10/12 px-44 " onSubmit={handleSubmit}>
             <input type="text" className="hidden" onChange={(e) => setHoney(e.target.value)} />
             <Input label="Name" placeholder="Enter your name" error={errors.name} onChange={(val) => setName(val)} />
             <Input label="Email" placeholder="Enter your email" error={errors.email} onChange={(val) => setEmail(val)} />
-            {/* <div>
-                <p className="text-gray-300 text-lg mb-1 focus:outline-none " >Message:</p>
-                <textarea className={clsx('h-96 rounded-md p-2 w-96', errors.message && 'border-2 border-red-600 ')} type="text" onChange={(e) => setMessage(e.target.value)} />
-                {errors.message && <p className="text-red-600">{errors.message}</p>}
-            </div> */}
-            <button type="submit" className="text-gray-200 full bg-gray-700 h-12 rounded-md " >press</button>
+            <TextArea label="Message" placeholder="Enter your message" error={errors.message} onChange={(val) => setMessage(val)} />
+            <button type="submit" className="text-gray-300 hover:text-gray-200 w-full h-12 rounded-md transition-all duration-200 hover:border-gray-500 border border-gray-600 " >Email me</button>
 
         </form>
     )
--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -2,13 +2,16 @@ import ContactForm from "./ContactForm"
 
 const ContactMe = () => {
   return (
-    <div className="flex flex-col font-poppins justify-around items-center gap-y-32 h-screen w-screen p-24 bg-neutral-800 text-gray-400">
+    <div className="font-poppins h-screen w-screen bg-neutral-800 text-gray-400">
       <div className="w-full">
-        <h1 className="text-center sm:text-8xl text-6xl font-extralight ">Contact me:</h1>
+        <h1 className="text-center sm:text-8xl py-20 text-6xl font-extralight ">Contact me:</h1>
       </div>
-      <div className="flex justify-center gap-x-10 w-full ">
+      <div className="flex justify-between divide-x divide divide-gray-400 w-full ">
         <ContactForm />
-        <p className="w-2/5 ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi aliquet, quam vitae fermentum viverra, magna purus lacinia quam, non ornare nulla nibh at ipsum. Quisque ut lorem leo. Nam diam lacus, dapibus sit amet pulvinar a, fermentum eget lorem. Nam vulputate rutrum consectetur. Nunc congue, nisi a egestas interdum, arcu leo maximus est, in eleifend metus mauris non quam. Curabitur in egestas nibh. Praesent id nibh tincidunt, scelerisque mi sed, scelerisque erat. Phasellus commodo, elit nec suscipit gravida, ligula erat tristique dui, id faucibus velit dolor nec mi. Cras vitae molestie dolor. Suspendisse quis dolor quis enim auctor porta non non nisi. Nunc auctor sit amet lacus sit amet placerat. Donec malesuada aliquam orci quis facilisis. Curabitur laoreet quam urna, sit amet consectetur elit facilisis tincidunt.</p>
+        <div className="w-3/12 flex flex-col items-center justify-center gap-y-28">
+          <li onClick={() => navigator.clipboard.writeText('+359 878 212 118')} className="hover:text-blue-400 cursor-pointer transition-all hover:-translate-y-2">+359 878 212 118</li>
+          <li onClick={() => navigator.clipboard.writeText('teodor.b.marinov@gmail.com')} className="hover:text-blue-400 cursor-pointer transition-all hover:-translate-y-2">teodor.b.marinov@gmail.com</li>
+        </div>
       </div>
     </div>
   )
