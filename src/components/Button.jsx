3b64bdf7fc172f45114ded059bad550692da3898
teoMarinov <teodor.b.marinov@gmail.com>
2024-03-06 19:27:22 +0200

    add load homepage animation

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,8 +3,8 @@ import clsx from "clsx"
 const Button = ({ text, open, onClick, textColor }) => {
     return (
         <button onClick={onClick} className={clsx(`group font-poppins p-3 text-lg rounded-md text-${textColor}`)}>
-            <p className="group-hover:-translate-y-2 transition-all duration-200 delay-500">{text}</p>
-            <p className={clsx("text-3xl group-hover:scale-125 scale-0 p-0 -m-4 transition-all duration-300 delay-100", open === text && 'scale-125')}>.</p>
+            <p className="group-hover:-translate-y-2 transition-all duration-200 delay-100">{text}</p>
+            <p className={clsx("text-3xl group-hover:scale-125 scale-0 p-0 -m-4 transition-all duration-300 delay-200", open === text && 'scale-125')}>.</p>
         </button>
     )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,35 +3,65 @@ import MobileDrawer from "./MobileDrawer"
 import { useState } from "react"
 import clsx from "clsx"
 import { Link } from 'react-scroll';
+import { motion } from 'framer-motion'
 
 const Navbar = () => {
 
   const [drawerOpen, setDrawerOpen] = useState(false);
   const [open, setOpen] = useState('Home');
 
-
   return (
     <>
-      <div className="hidden z-50 lg:flex fixed p-3 px-5 w-full justify-between">
+      <div className="hidden z-50 lg:flex fixed p-3 px-5 w-full justify-between bg-gradient-to-b from-zinc-800 to-black/0">
 
         <span className="gap-x-8 flex">
-          <Link activeClass="active" to="home" spy={true} smooth={true} duration={500} isDynamic={true}>
-            <Button text={'Home'} open={open} textColor={'gray-900'} onClick={() => setOpen('Home')} />
-          </Link>
-          <Link activeClass="active" to="aboutMe" spy={true} smooth={true} duration={500}>
-            <Button text={'About me'} open={open} textColor={'gray-900'} onClick={() => setOpen('About me')} />
-          </Link>
+          <motion.div
+            initial={{ y: ['100vh'] }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Link activeClass="active" to="home" spy={true} smooth={true} duration={500} isDynamic={true}>
+              <Button text={'Home'} open={open} textColor={'white'} onClick={() => setOpen('Home')} />
+            </Link>
+          </motion.div>
+          <motion.div
+            initial={{ y: ['100vh'] }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: .4 }
+            }>
+            <Link activeClass="active" to="aboutMe" spy={true} smooth={true} duration={500}>
+              <Button text={'About me'} open={open} textColor={'white'} onClick={() => setOpen('About me')} />
+            </Link>
+          </motion.div>
         </span>
         <span className="gap-x-8 flex">
-          <Link activeClass="active" to="skills" spy={true} smooth={true} duration={500} >
-            <Button text={'Skills'} open={open} textColor={'gray-900'} onClick={() => setOpen('Skills')} />
-          </Link>
-          <Link activeClass="active" to="projects" spy={true} smooth={true} duration={500} >
-            <Button text={'Projects'} open={open} textColor={'gray-900'} onClick={() => setOpen('Projects')} />
-          </Link>
-          <Link activeClass="active" to="contactMe" spy={true} smooth={true} duration={500} >
-            <Button text={'Contact me'} open={open} textColor={'gray-900'} onClick={() => setOpen('Contact me')} />
-          </Link>
+          <motion.div
+            initial={{ y: ['100vh'] }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: .2 }
+            }>
+            <Link activeClass="active" to="skills" offset={-100} spy={true} smooth={true} duration={500} >
+              <Button text={'Skills'} open={open} textColor={'white'} onClick={() => setOpen('Skills')} />
+            </Link>
+          </motion.div>
+          <motion.div
+            initial={{ y: ['100vh'] }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: .8 }
+            }>
+            <Link activeClass="active" to="projects" spy={true} smooth={true} duration={500} >
+              <Button text={'Projects'} open={open} textColor={'white'} onClick={() => setOpen('Projects')} />
+            </Link>
+          </motion.div>
+          <motion.div
+            initial={{ y: ['100vh'] }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: .6 }
+            }>
+            <Link activeClass="active" to="contactMe" offset={60} spy={true} smooth={true} duration={500} >
+              <Button text={'Contact me'} open={open} textColor={'white'} onClick={() => setOpen('Contact me')} />
+            </Link>
+          </motion.div>
         </span>
       </div >
       <div className="w-full fixed xl:hidden flex justify-end z-50">
