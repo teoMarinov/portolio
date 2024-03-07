b71d09d695f0406f0c0175ee6b13bc2822790676
teoMarinov <teodor.b.marinov@gmail.com>
2024-03-07 17:37:19 +0200

    add contact me paralax

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -1,18 +1,36 @@
 import ContactForm from "./ContactForm"
 import ContactList from "./ContactList"
-import {Element} from 'react-scroll';
+import { Element } from 'react-scroll';
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
+
 
 const ContactMe = () => {
+
+  const ref = useRef(null)
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ['0 1', '1 1'],
+  })
+
+  const scroll = useTransform(scrollYProgress, [0, 1], ["50%", "0%"])
+
+
   return (
-    <Element name='contactMe' className="font-poppins h-full w-screen pb-10 bg-neutral-800 text-gray-400">
-      <div className="w-full">
-        <h1 className="text-center py-20 sm:text-8xl text-6xl font-extralight ">Contact me:</h1>
-      </div>
-      <div className="flex xl:flex-row flex-col-reverse justify-between xl:divide-x  divide-gray-400 w-full xl:items-stretch items-center ">
-       <ContactForm />
-       <ContactList />
-      </div>
-    </Element>
+    <div ref={ref}>
+      <Element name='contactMe' className="font-poppins h-full w-screen pb-10 bg-neutral-800 text-gray-400">
+        <motion.div style={{ translateY: scroll }}>
+          <div className="w-full">
+            <h1 className="text-center py-20 sm:text-8xl text-6xl font-extralight ">Contact me:</h1>
+          </div>
+          <div className="flex xl:flex-row flex-col-reverse justify-between xl:divide-x divide-gray-400 w-full xl:items-stretch items-center ">
+            <ContactForm />
+            <ContactList />
+          </div>
+        </motion.div>
+      </Element>
+    </div >
   )
 }
 
