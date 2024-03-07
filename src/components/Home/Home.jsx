ec8dd6352be6b9816ce35cea28765eab54c95d8b
teoMarinov <teodor.b.marinov@gmail.com>
2024-03-07 23:53:08 +0200

    add scroll down to scroll button on home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,6 +7,7 @@ import { MdOutlineKeyboardDoubleArrowDown } from "react-icons/md";
 import { Element } from 'react-scroll';
 import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
+import { Link } from 'react-scroll';
 
 const Home = () => {
 
@@ -93,17 +94,19 @@ const Home = () => {
                         initial={{ y: ['100vh'] }}
                         animate={{ opacity: 1, y: 0 }}
                         transition={{ duration: .3, delay: 1.1, ease: 'circOut' }}
-                        className="hidden border-4 border-gray-100 w-12 h-24 rounded-full xl:flex flex-col items-center xl:translate-y-0 translate-y-10 justify-center overflow-hidden"
+                        className="hidden border-4 cursor-pointer border-gray-100 w-12 h-24 rounded-full xl:flex flex-col items-center xl:translate-y-0 translate-y-10 justify-center overflow-hidden"
                     >
-                        <motion.div
-                            animate={{
-                                y: [-60, 60],
-                                opacity: [0, 1, 0]
-                            }}
-                            transition={{ duration: 1.6, repeat: Infinity }}
-                        >
-                            <MdOutlineKeyboardDoubleArrowDown size={42} className="text-gray-100" />
-                        </motion.div>
+                        <Link activeClass="active" to="aboutMe" spy={true} smooth={true} duration={500}>
+                            <motion.div
+                                animate={{
+                                    y: [-60, 60],
+                                    opacity: [0, 1, 0]
+                                }}
+                                transition={{ duration: 1.6, repeat: Infinity }}
+                            >
+                                <MdOutlineKeyboardDoubleArrowDown size={42} className="text-gray-100" />
+                            </motion.div>
+                        </Link>
                     </motion.div>
                 </motion.div>
             </Element>
