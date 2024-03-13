63275041432e7fdef8b6f5b8a84831ace81966a9
teoMarinov <teodor.b.marinov@gmail.com>
2024-03-13 15:16:41 +0200

    add loading cursors

--- src/components/AnimatedPage.jsx
+++ src/components/AnimatedPage.jsx
@@ -17,7 +17,7 @@ const AnimatedPage = ({ children, text, display = true }) => {
                         transition: { duration: .3 }
                     }
                 }
-                className={clsx('fixed w-screen h-screen bg-zinc-800 justify-center items-center z-50 text-white text-center sm:text-6xl text-4xl font-light font-poppins', display ? " flex" : "hidden")}
+                className={clsx('fixed w-screen h-screen cursor-wait bg-zinc-800 justify-center items-center z-50 text-white text-center sm:text-6xl text-4xl font-light font-poppins', display ? " flex" : "hidden")}
             >
                 {text}
             </motion.div>
